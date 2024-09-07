e6697679ed9050b63d5ac601117173ea6829200d
Florian Jafar Sabonchi <sabonchi@posteo.de>
2024-09-07 18:41:30 +0200

    feat: first draft for map_screen.dart

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -9,11 +10,43 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
 
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-          child: Text("TODO MapScreen"),
+    return Scaffold(
+        body: GoogleMap(
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          compassEnabled: true,
+          markers: <Marker>{
+            // TODO: replace with actual markers
+            Marker(
+              onTap: () {
+                print('Marker tapped');
+              },
+              markerId: const MarkerId('1'),
+              position: const LatLng(49.4875, 8.4649),
+              infoWindow: const InfoWindow(
+                title: 'Heidelberg',
+                snippet: 'Germany',
+              ),
+            ),
+            Marker(
+              onTap: () {
+                print('Marker tapped');
+              },
+              markerId: const MarkerId('1'),
+              position: const LatLng(49.4875, 8.4750),
+              infoWindow: const InfoWindow(
+                title: 'Heidelberg',
+                snippet: 'Germany',
+              ),
+            ),
+          },
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(49.4875, 8.4649),
+            zoom: 14.4746,
+          ),
         )
     );
   }
