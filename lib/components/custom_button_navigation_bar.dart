ef0f869c1f49b6b2e169281b9bb6bff09b8aafa1
lgrauer <redneal314@prontonmail.com>
2024-09-07 13:50:35 +0200

    custom_button_navigation_bar done (hopefully)

---
+++ lib/components/custom_button_navigation_bar.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/project_constants.dart';
+
+class CustomBottomNavigationBar extends BottomNavigationBar {
+  CustomBottomNavigationBar({
+    super.key,
+    required super.currentIndex,
+    required super.onTap,
+    required NavigationBarType type
+  }) : super(
+    type: BottomNavigationBarType.fixed,
+    selectedItemColor: ProjectConstants.selectionColorNavigationBar,
+    unselectedItemColor: ProjectConstants.unselectedColorNavigationBar,
+    showSelectedLabels: true,
+    showUnselectedLabels: true,
+    items: _getItemsForType(type),
+  );
+
+  static List<BottomNavigationBarItem> _getItemsForType(NavigationBarType type) {
+    switch (type) {
+      case NavigationBarType.customer:
+        return _customerItems;
+      case NavigationBarType.producer:
+        return _producerItems;
+      case NavigationBarType.delivery:
+        return _deliveryItems;
+      default:
+        return _noAuthItems;
+    }
+  }
+
+  static const List<BottomNavigationBarItem> _noAuthItems = [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.add_business_outlined),
+      activeIcon: Icon(Icons.add_business),
+      label: "Become a Producer"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.content_paste_outlined),
+        activeIcon: Icon(Icons.content_paste),
+        label: "Orders"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.shopping_bag_outlined),
+        activeIcon: Icon(Icons.shopping_bag),
+        label: "Shopping"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.shopping_cart_outlined),
+        activeIcon: Icon(Icons.shopping_cart),
+        label: "Shopping Cart"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.login_outlined),
+        activeIcon: Icon(Icons.login),
+        label: "Login"
+    )
+  ];
+  static const List<BottomNavigationBarItem> _customerItems = [
+    BottomNavigationBarItem(
+        icon: Icon(Icons.add_business_outlined),
+        activeIcon: Icon(Icons.add_business),
+        label: "Become a Producer"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.content_paste_outlined),
+        activeIcon: Icon(Icons.content_paste),
+        label: "Orders"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.shopping_bag_outlined),
+        activeIcon: Icon(Icons.shopping_bag),
+        label: "Shopping"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.shopping_cart_outlined),
+        activeIcon: Icon(Icons.shopping_cart),
+        label: "Shopping Cart"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.person_outlined),
+        activeIcon: Icon(Icons.person),
+        label: "Profile"
+    )
+  ];
+  static const List<BottomNavigationBarItem> _producerItems = [
+    BottomNavigationBarItem(
+        icon: Icon(Icons.content_paste_outlined),
+        activeIcon: Icon(Icons.content_paste),
+        label: "Your Articles"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.inventory_2_outlined),
+        activeIcon: Icon(Icons.inventory_2),
+        label: "Your Stock"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.person_outlined),
+        activeIcon: Icon(Icons.person),
+        label: "Profile"
+    )
+  ];
+  static const List<BottomNavigationBarItem> _deliveryItems = [
+    BottomNavigationBarItem(
+        icon: Icon(Icons.assignment_outlined),
+        activeIcon: Icon(Icons.assignment),
+        label: "Current Step"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.local_shipping_outlined),
+        activeIcon: Icon(Icons.local_shipping),
+        label: "Tour"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.person_outlined),
+        activeIcon: Icon(Icons.person),
+        label: "Profile"
+    )
+  ];
+}
+
+enum NavigationBarType {
+  noAuth,
+  customer,
+  producer,
+  delivery
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/components/custom_button_navigation_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-   int _selectedIndex = 0;
+  int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -46,36 +47,10 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Container(),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: const Color(0xFF016905),
-        unselectedItemColor: const Color(0xFF636363),
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
+      bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart_outlined),
-            label: 'Shopping Cart',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag_outlined),
-            label: 'Product overview',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.inventory_2_outlined),
-            label: 'Inventory',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_shipping),
-            label: 'Become a producer',
-          ),
-        ],
-      ),
+        type: NavigationBarType.noAuth),
     );
   }
 }
